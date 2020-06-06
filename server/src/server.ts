839cd765f0d1858429b19a00a1f4eca9363c89cd
PedroLucasSS <pedro.lucas4431@gmail.com>
2020-06-05 23:26:25 -0300

    adicionando upload de imagem

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,8 @@ import path from 'path'
 
 import routes from './routes'
 
+import { errors } from 'celebrate'
+
 const app = express()
 
 app.use(cors())
@@ -13,5 +15,7 @@ app.use(express.json())
 app.use(routes)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
+app.use(errors())
 
 app.listen(3333)
